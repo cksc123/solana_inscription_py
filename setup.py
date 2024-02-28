bed04ec47535e282899f6f5b284ed5d755b62827
cksc123 <128780066+cksc123@users.noreply.github.com>
2024-02-28 18:36:02 +1300

    add project_urls.source

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='solana_inscription_py',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=["solana <= 0.30.2"],
     description='Mint Solana Inscriptions',
@@ -11,6 +11,9 @@ setup(
     author='CK',
     author_email='admin@example.com',
     url='https://github.com/cksc123/solana_inscription_py',
+    project_urls={
+        'source': 'https://github.com/cksc123/solana_inscription_py'
+    },
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
